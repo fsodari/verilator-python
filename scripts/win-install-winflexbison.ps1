63372468bf5d2423d1be9a0cc6621660de2eb420
fsodari <franksodari@gmail.com>
2024-12-14 14:41:52 -0700

    ...

--- scripts/win-install-winflexbison.ps1
+++ scripts/win-install-winflexbison.ps1
@@ -1,4 +1,4 @@
 git clone --depth 1 https://github.com/lexxmark/winflexbison extern/winflexbison
-cmake -S extern/winflexbison -B extern/winflexbison/build --install-prefix $PWD/extern/winflexbison/install
+cmake -S extern/winflexbison -B extern/winflexbison/build --install-prefix $PWD/.ccache/winflexbison
 cmake --build extern/winflexbison/build --config Release
-cmake --install extern/winflexbison/build --prefix $PWD/extern/winflexbison/install
+cmake --install extern/winflexbison/build --prefix $PWD/.ccache/winflexbison
